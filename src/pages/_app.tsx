2435284337035b52452e9ac9da1ddc3573084103
Wilson Castro <wilsoncastro0710@gmail.com>
2021-01-22 11:08:31 -0500

    PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,33 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     <>
       <GlobalStyle />
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/manifest/apple-icon-120x120-dunplab-manifest-33222.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/manifest/favicon-32x32-dunplab-manifest-33222.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/manifest/favicon-16x16-dunplab-manifest-33222.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/manifest/favicon-96x96-dunplab-manifest-33222"
+        />
+        <link rel="manifest" href="/manifest/manifest.json" />
+        <meta name="msapplication-TileColor" content="#0000" />
+        <meta name="theme-color" content="#0000" />
+
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
